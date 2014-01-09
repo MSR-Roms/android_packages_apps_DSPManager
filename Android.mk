ff940515225d2a71061e640786740d525a066084
spacecaker <robin-barry@hotmail.com>
2014-01-09 14:11:04 +0100

    fix anroidmk

--- Android.mk
+++ Android.mk
@@ -5,7 +5,10 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    sandroid-support-v4 \
+    sandroid-support-v13
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -15,5 +18,10 @@ LOCAL_OVERRIDES_PACKAGES := MusicFX
 
 include $(BUILD_PACKAGE)
 
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+     sandroid-support-v13:libs/sandroid-support-v13.jar \
+     sandroid-support-v4:libs/sandroid-support-v4.jar
+
+include $(BUILD_MULTI_PREBUILT)
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
